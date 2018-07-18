6beb96dcddac70becb1dce5a1f9b52802a31c532
zaoldyeck <zaoldyeck9970@gmail.com>
2018-07-18 18:56:25 +0800

    Add config file

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    messenger: {
+        accessToken: '',
+        appSecret: '',
+        verifyToken: '',
+    },
+    line: {
+        channelSecret: '',
+        accessToken: '+Ix99B4EaSnf1uUmRhnjr+TQNKr+LvVdhX5QeHwBfQWoiM1lASOyHWHcDWZv3RARRrTDsnJ2RMO/y0SdpklPZVUbOG+SRJs2ZtQy8+alxikUGXPhW5+W9piAdB04t89/1O/w1cDnyilFU='
+    },
+    telegram: {
+        accessToken: '',
+    },
+    olami: {
+        appKey: '',
+        appSectet: ''
+    },
+    kkbox: {
+        id: '',
+        secret: ''
+    }
+}
--- src/handler.js
+++ src/handler.js
@@ -1,24 +1,26 @@
 const {LineHandler, MessengerHandler} = require('bottender')
 const olami = require('./nlp/Olami')
 
+const helpMessage = 'Hi~ 本 Bot 是用 http://bit.ly/2uz7wY4 開源程式碼打造\n\n' +
+    '您可以問我\n' +
+    '天氣：「台北天氣如何」\n' +
+    '百科：「川普是誰」\n' +
+    '新聞：「今日新聞」\n' +
+    '音樂：「我想聽周杰倫的等你下課」；「播放告白氣球」；「播放動漫歌曲類型的歌」\n' +
+    '日曆：「現在時間」\n' +
+    '詩詞：「我想聽水調歌頭這首詩」\n' +
+    '笑話：「講個笑話」\n' +
+    '故事：「說個故事」\n' +
+    '股票：「台積電的股價」\n' +
+    '食譜：「蛋炒飯怎麼做」\n' +
+    '聊天：「你好嗎」'
+
 exports.lineHandler = new LineHandler()
     .onFollow(async context => {
-        await context.replyText('歡迎你')
+        await context.replyText(helpMessage)
     })
     .onText('\/help', async context => {
-        await context.replyText('Hi~ 本 Bot 是用 http://bit.ly/2uz7wY4 開源程式碼打造\n\n' +
-            '您可以問我\n' +
-            '天氣，例如：「台北天氣如何」\n' +
-            '百科，例如：「川普是誰」\n' +
-            '新聞，例如：「今日新聞」\n' +
-            '音樂，例如：「我想聽周杰倫的等你下課」\n' +
-            '日曆，例如：「現在時間」\n' +
-            '詩詞，例如：「我想聽水調歌頭這首詩」\n' +
-            '笑話，例如：「講個笑話」\n' +
-            '故事，例如：「說個故事」\n' +
-            '股票，例如：「台積電的股價」\n' +
-            '食譜，例如：「蛋炒飯怎麼做」\n' +
-            '聊天，例如：「你好嗎」')
+        await context.replyText(helpMessage)
     })
     .onText(async context => {
             const text = context.event.text
@@ -31,6 +33,9 @@ exports.lineHandler = new LineHandler()
     })
 
 exports.messengerHandler = new MessengerHandler()
+    .onText('\/help', async context => {
+        await context.replyText(helpMessage)
+    })
     .onText(async context => {
         const text = context.event.text
         const reply = await olami.nli(text)
